af5f79c10f0bcfcb17f55762eb40b4f2543ae096
YunjooCho <rollcake0317@naver.com>
2022-08-06 16:55:45 +0900

    2022/08/06

---
+++ No11654.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char n;
+
+    scanf("%c", &n);
+    if ((n >= 'A' && n <= 'Z') || (n >= 'a' && n <= 'z') || (n >= '0' && n <= '9'))
+    {
+        printf("%d", n);
+    }
+    return (0);
+}
