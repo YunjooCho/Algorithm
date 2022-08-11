ad43cd23cf6ec5b1c7887945a3ba37045f5b8ea9
YunjooCho <rollcake0317@naver.com>
2022-08-11 23:36:22 +0900

    2022/08/11

--- No9498.c
+++ No9498.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    int score;
-    
+    int score = 0;
+
     scanf("%d", &score);
-    if(score>=90)
+    if (score >= 90)
+    {
         printf("A");
-    else if(score>=80)
+    }
+    else if (score >= 80)
+    {
         printf("B");
-    else if(score>=70)
+    }
+    else if (score >= 70)
+    {
         printf("C");
-    else if(score>=60)
+    }
+    else if (score >= 60)
+    {
         printf("D");
+    }
     else
+    {
         printf("F");
-    return 0;
+    }
+    return (0);
 }
