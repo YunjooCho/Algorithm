72f2839b1ef344d833175848885dc64d67842111
Yunjoo Cho <yunjcho@cx1r4s8.42seoul.kr>
2023-05-30 16:59:53 +0900

    No10866.py

--- BOJ/No10866.py
+++ BOJ/No10866.py
@@ -2,18 +2,42 @@ from collections import deque
 import sys
 
 deque = deque()
-com, arg = int(sys.stdin.readline().split())
-print(com)
-print(arg)
-
-def push_front(X) :
-	deque.appendleft(X)
-
-def push_back(X) :
-	deque.append(X)
-
-def pop_front() :
-	deque.popleft()
-
-def pop_back() :
-	deque.pop()
+cnt = int(sys.stdin.readline())
+for _ in range(cnt) :
+	inputStr = sys.stdin.readline().rstrip().split()
+	if len(inputStr) == 1 :
+		com = inputStr[0]
+	else :
+		com, X = inputStr
+  
+	if com == 'push_front' :
+		deque.appendleft(X)
+	elif com == 'push_back' :
+		deque.append(X)
+	elif com == 'pop_front' :
+		if (len(deque) == 0) :
+			print(-1)
+		else :
+			print(deque.popleft())
+	elif com == 'pop_back' :
+		if (len(deque) == 0) :
+			print(-1)
+		else :
+			print(deque.pop())
+	elif com == 'size' :
+		print(len(deque))
+	elif com == 'empty' :
+		if (len(deque) == 0) :
+			print(1)
+		else :
+			print(0)
+	elif com == 'front' :
+		if (len(deque) == 0) :
+			print(-1)
+		else :
+			print(deque[0])
+	elif com == 'back' :
+		if (len(deque) == 0) :
+			print(-1)
+		else :
+			print(deque[len(deque) - 1])
