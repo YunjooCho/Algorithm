ed811924bdf7713d004562a59d0f0a811971c28a
Yunjoo Cho <yunjcho@c1r7s6.42seoul.kr>
2023-08-08 12:48:34 +0900

    backup

--- BOJ/No11660.py
+++ BOJ/No11660.py
@@ -6,4 +6,9 @@ N, M = map(int, sys.stdin.readline().split())
 numbers = []
 sum = 0
 for n in range(1, N + 1) :
-    list(map(int, sys.stdin.readline().split()))
+    numbers.append(list(map(int, sys.stdin.readline().split())))
+
+for j in range() : 
+print(numbers)
+print(numbers[2][2]) # 인덱스
+print(numbers[0][0])
