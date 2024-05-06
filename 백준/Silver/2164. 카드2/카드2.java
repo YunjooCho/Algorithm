bcf18e6e3a3b2d02a0e2849844e5a6daca517e97
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-05-06 15:10:37 +0900

    [Silver IV] Title: 카드2, Time: 292 ms, Memory: 39924 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+
+			Scanner sc = new Scanner(System.in);
+			int n = sc.nextInt();
+			int flag = 1;
+
+			Deque<Integer> deq = new ArrayDeque<Integer>();
+			for (int i = 1; i <= n; i++) {
+				deq.addLast(i);
+			}
+
+			while (deq.size() >= 1) {
+				if (deq.size() == 1) {
+					System.out.println(deq.getFirst());
+					break;
+				}
+				if (flag % 2  == 0) {
+					int val = deq.removeFirst();
+					deq.addLast(val);
+				} else {
+					deq.removeFirst();
+				}
+				flag++;
+			}
+			sc.close();
+	}
+}
