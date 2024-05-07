2c5816d4769d51d87605d8da0db620065716188e
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-05-07 18:08:56 +0900

    [Silver II] Title: 최소 힙, Time: 1812 ms, Memory: 114472 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+		PriorityQueue<Integer> priorityQue = new PriorityQueue<Integer>();
+
+		int n = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			int elem = scan.nextInt();
+			if (elem == 0) {
+				if (priorityQue.isEmpty()) {
+					System.out.println("0");
+				} else {
+					System.out.println(priorityQue.poll());
+				}
+			} else {
+				priorityQue.offer(elem);
+			}
+		}
+		scan.close();
+	}
+}
