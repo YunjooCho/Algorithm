6570a9208f93369e226d787d675e6337f3cfffdf
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-05-21 23:50:19 +0900

    No5800.java BOJ Sort

---
+++ BOJ/No5800.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class No5800 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int idx = 1;
+		int cnt = scan.nextInt();
+		int dis = 0;
+
+		for (int i = 0; i < cnt; i++) {
+			int num = Integer.parseInt(scan.next());
+			List<Integer> numList = new ArrayList<Integer>();
+			for (int j = 0; j < num; j++) {
+				int elem = Integer.parseInt(scan.next());
+				numList.add(elem);
+			}
+			Collections.sort(numList);
+			for (int j = 0; j < num - 1; j++) {
+				int cal = numList.get(j + 1) - numList.get(j);
+				if (numList.get(j + 1) - numList.get(j) > dis) {
+					dis = cal;
+				}
+			}
+			System.out.println("Class " + idx);
+			int maxNum = numList.get(numList.size() - 1);
+			int minNum = numList.get(0);
+			System.out.println("Max " + maxNum + ", Min " + minNum + ", Largest gap " + dis);
+			
+			idx++;
+			dis = 0;
+		}
+		scan.close();
+	}
+}
