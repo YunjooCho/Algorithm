1787572025dfd352542c5f8f6a16c77bbc140a94
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-05-24 20:57:46 +0900

    [Gold II] Title: 버블 소트, Time: 1748 ms, Memory: 83808 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+		int max = 0;
+
+		Nums[] A = new  Nums[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = new Nums();
+			A[i].index = i;
+			A[i].value = Integer.parseInt(reader.readLine());
+		}
+ 
+		Arrays.sort(A);
+
+		for (int i = 0; i < N; i++) {
+			int dist = A[i].index - i;
+			if (dist > max) {
+				max = dist;
+			}
+		}
+		System.out.println(max + 1);
+	}
+}
+
+class Nums implements Comparable<Nums>{
+	int value;
+	int index;
+
+	@Override
+	public int compareTo(Nums n) {
+			return this.value - n.value;
+	}
+}
